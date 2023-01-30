cbea63e3e31c3c4b709b238ed42c9a213d2242b4
Karsten Dinsmore <kdinsmor@calpoly.edu>
2023-01-29 20:19:26 -0800

    linking

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,38 +1,73 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table';
 import Form from './Form';
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-  // const [characters, setCharacters] = useState([
-  //   {
-  //     name: 'Charlie',
-  //     job: 'Janitor',
-  //   },
-  //   {
-  //     name: 'Mac',
-  //     job: 'Bouncer',
-  //   },
-  //   {
-  //     name: 'Dee',
-  //     job: 'Aspring actress',
-  //   },
-  //   {
-  //     name: 'Dennis',
-  //     job: 'Bartender',
-  //   },
-  // ]);
 
-  function removeOneCharacter (index) {
-    const updated = characters.filter((characters, i) => {
-      return i !== index
-    });
-    setCharacters(updated);
+  async function makeDeleteCall(ID) {
+    try {    
+      const response = await axios.delete(`http://localhost:5000/users/${ID}`);
+      return response;     
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    } 
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function removeOneCharacter (index) {
+    const id = characters[index].id;
+    makeDeleteCall(id).then( result => {
+      if (result && result.status === 204) {
+        const updated = characters.filter((characters, i) => {
+          return i !== index
+        })
+        setCharacters(updated);
+      }
+    });
+  }
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, [] );
+
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+      if (result && result.status === 201) {
+        person.id = result.data;
+        setCharacters([...characters, person]);
+      }
+    });
   }
 
   return (
@@ -42,7 +77,5 @@ function MyApp() {
     </div> 
   )
 }
-
-
 
 export default MyApp;
